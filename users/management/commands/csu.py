86e8acc46f3890d8ceee264963471abc1f96c2fc
jylu lyapunova <jylilapa@yandex.ru>
2025-06-03 14:23:36 +0300

    add fixed

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -5,7 +5,7 @@ from users.models import User
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        user = User.objects.create(email="jylilapa@yandex.ru")
+        user = User.objects.create(email="1jylilapa@yandex.ru")
         user.set_password("CV_5fhK9")
         user.is_staff = True
         user.is_active = True
